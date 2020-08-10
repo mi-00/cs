49df1652e957f7e0c7725c795004769a4f52d3fc
mi_00 <mihaela.benea@tivo.com>
2020-08-10 16:59:34 +0200

    Add project files.

---
+++ ConsoleApp1/Interface1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    interface IVehicle
+    {
+        string Location 
+        { 
+            get;
+        }
+        int NumberOfSeats 
+        { 
+            get;
+        }
+        void MoveTo (string City);
+    }
+
+    class Car : IVehicle
+    {
+        //constructor
+        public Car(String carLocation, int noOfSeats)
+        {
+            Location = carLocation;
+            NumberOfSeats = 5;
+        }
+        public int NumberOfSeats
+        {
+            get;
+            set;
+        }
+        // MoveTo('Luxembourg') changes Location property
+
+
+        public string Location
+        {
+            get;
+            set;
+        }
+        public void MoveTo(string City)
+        {
+            this.Location = City;
+        }
+
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car audi = new Car("Hesperange", 8);
+
+            Console.WriteLine("Hello Mihaela!");
+            Console.WriteLine("car location + car number of seats: " + audi.Location + " + " + audi.NumberOfSeats);
+
+            audi.MoveTo("Luxembourg");
+            Console.WriteLine("car location + car number of seats: " + audi.Location + " + " + audi.NumberOfSeats);
+
+        }
+    }
+}
